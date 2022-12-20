e63d39c4d4d07e54fab0e64229b187eab8823d91
DalatCoder <hieuntctk42@gmail.com>
2022-12-20 12:20:54 +0700

    hash md5

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,10 +1,10 @@
 defmodule Identicon do
   def main(input) do
     input
-    |> has_input()
+    |> hash_input()
   end
 
-  def has_input(input) do
+  def hash_input(input) do
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list()
   end
